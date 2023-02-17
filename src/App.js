2a785eba0e40ed60ed23d2435cf370dfb8e0c3f2
Mikhail <mikhail.olegovich.bessonov@gmail.com>
2023-02-17 17:33:43 +0500

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{}</h1>
         <button className="minus">- Минус</button>
         <button className="plus">Плюс +</button>
       </div>
